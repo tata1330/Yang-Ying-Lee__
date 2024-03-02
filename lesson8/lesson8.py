aa15b6f3508d0762a191cf70145a3fb42ece8088
李陽瑩 <157937325+tata1330@users.noreply.github.com>
2024-03-02 20:03:02 +0000

    modify

---
+++ lesson8/lesson8.py
@@ -0,0 +1,34 @@
+import random
+
+min = 1
+max = 100
+guess = random.randint(min,max)
+count = 0
+
+print("========猜數字遊戲=========\n\n")
+#print(f"要猜的數字是{guess}")
+
+while(True):
+    try:
+        keyin = int(input(f"猜數字範圍{min}~{max}"))
+        count += 1
+        if max >= keyin >=min:
+            if keyin == guess:
+                print(f"恭喜你猜對了，答案就是{guess}")
+                print(f"你已經猜了{count}次了")
+                break
+            elif keyin >= guess:
+                print("再小一點")
+                max = keyin - 1
+            elif keyin <= guess:
+                print("再大一點")
+                min = keyin + 1
+        else:
+            print("超出範圍")
+    except:
+        print("格式錯誤")
+        count += 1
+        
+    print(f"你已經猜了{count}次了")
+
+print("遊戲結束")
